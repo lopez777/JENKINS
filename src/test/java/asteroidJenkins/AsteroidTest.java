6a42df5c1a376e2ef8fcd67b5915aaf76be65af6
lopez816 <lopez816@cougars.csusm.edu>
2021-12-02 13:51:56 -0800

    First commit

---
+++ src/test/java/asteroidJenkins/AsteroidTest.java
@@ -0,0 +1,15 @@
+package asteroidJenkins;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.psnbtech.Game;
+
+public class AsteroidTest {
+
+	@Test
+	public void testGetHello() {
+		Game g = new Game();
+		assertEquals("Testing hello with Jenkins!", g.getHello());
+	}
+
+}
