c9fce83ec7735d499cd5dbf2dec98f146dc6e814
Marcos Lopez <71795774+lopez777@users.noreply.github.com>
2021-12-02 16:37:34 -0800

    testing email

--- src/test/java/asteroidJenkins/AsteroidTest.java
+++ src/test/java/asteroidJenkins/AsteroidTest.java
@@ -1,5 +1,5 @@
 package asteroidJenkins;
-// testing with asteroid jenkins project
+// testing with asteroid jenkins project email plugin
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.psnbtech.Game;
