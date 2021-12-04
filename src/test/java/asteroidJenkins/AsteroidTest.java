5b0369da293f6668cc1b6edc6d71dfae3e687b36
Marcos Lopez <71795774+lopez777@users.noreply.github.com>
2021-12-03 23:01:24 -0800

    video demo

--- src/test/java/asteroidJenkins/AsteroidTest.java
+++ src/test/java/asteroidJenkins/AsteroidTest.java
@@ -2,10 +2,12 @@ package asteroidJenkins;
 // testing email noti for multiple recipientss
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.psnbtech.Game;
 
 public class AsteroidTest {
 
+	@Ignore
 	@Test
 	public void testGetHello() {		
 		Game g = new Game();
